8c4c080bdbab09019bd79df3cc8b1846984a0bd5
Yazdun <yazduntube@gmail.com>
2022-04-12 21:30:12 +0800

    general : made progress

--- components/contactForm/index.jsx
+++ components/contactForm/index.jsx
@@ -4,21 +4,46 @@ import { useForm, FormProvider } from 'react-hook-form'
 import { EmailInput, isError, MessageInput, NameInput } from 'utils'
 import { SuccessModal } from './successModal'
 import { useState } from 'react'
+import axios from 'axios'
+import '/axios'
 
 export const ContactForm = () => {
   const [show, setShow] = useState(false)
   const [loading, setLoading] = useState(false)
   const [email, setEmail] = useState(null)
+  const [serverErrors, setServerErrors] = useState()
+
+  const execute = async data => {
+    setLoading(true)
+    setServerErrors(null)
+    setShow(false)
+    try {
+      await axios
+        .post('/message/create', data)
+        .then(res => {
+          setEmail(res.data.message.email)
+          setShow(true)
+          console.log(res.data)
+          setServerErrors(null)
+          methods.reset()
+        })
+        .then(() => setLoading(false))
+    } catch (error) {
+      setServerErrors(error.response.data.msg.split(','))
+      setLoading(false)
+      console.log(error)
+    }
+  }
 
   const methods = useForm()
   return (
     <>
       <FormProvider {...methods}>
-        <form onSubmit={methods.handleSubmit(data => console.log(data))}>
+        <form onSubmit={methods.handleSubmit(data => execute(data))}>
           <Input {...EmailInput} />
           <Input {...NameInput} />
           <Textarea {...MessageInput} />
-          {/* <ServerErrors errors={serverErrors} /> */}
+          <ServerErrors errors={serverErrors} />
           <Button
             active
             fullwidth
